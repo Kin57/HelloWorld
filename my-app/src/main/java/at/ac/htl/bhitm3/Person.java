39383335239a7c915789f72415eb06bd7beb3b9f
IT210190 <113981352+IT210190@users.noreply.github.com>
2023-11-22 14:49:32 +0100

    Update Person

--- my-app/src/main/java/at/ac/htl/bhitm3/Person.java
+++ my-app/src/main/java/at/ac/htl/bhitm3/Person.java
@@ -1,4 +1,31 @@
 package at.ac.htl.bhitm3;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String address;
+
+    public Person(String firstName, String lastName, String dateOfBirth, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
