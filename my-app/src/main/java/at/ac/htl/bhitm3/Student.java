98fd185e942e7078d5cff3e5ed3bc0a712d0b617
Kin57 <a.h.kreuzer@gmail.com>
2023-11-22 14:53:52 +0100

    student

--- my-app/src/main/java/at/ac/htl/bhitm3/Student.java
+++ my-app/src/main/java/at/ac/htl/bhitm3/Student.java
@@ -1,5 +1,8 @@
 package at.ac.htl.bhitm3;
 
 public class Student extends Person{
-    
+
+    public Student(String firstName, String lastName, String dateOfBirth, String address) {
+        super(firstName, lastName, dateOfBirth, address);
+    }
 }
