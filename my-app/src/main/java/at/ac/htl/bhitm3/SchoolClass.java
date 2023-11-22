06ff0ec9c2a92817d338897af548936c97f37173
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2023-11-22 14:37:11 +0100

    schoolclass

--- my-app/src/main/java/at/ac/htl/bhitm3/SchoolClass.java
+++ my-app/src/main/java/at/ac/htl/bhitm3/SchoolClass.java
@@ -4,7 +4,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class SchoolClass {
-    public List getStudents() {
-        return new LinkedList<String>();
+
+    private List<Student> student_list = new LinkedList<>();
+
+    public List<Student> getStudent_list() {
+        return student_list;
     }
+
+    public void setStudent_list(List<Student> student_list) {
+        this.student_list = student_list;
+    }
+
+    
 }
