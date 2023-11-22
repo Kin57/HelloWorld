fe79914d3366b9927c43ff36d654c74f19e39bcf
Kin57 <a.h.kreuzer@gmail.com>
2023-11-22 14:57:48 +0100

    student

--- my-app/src/test/java/at/ac/htl/bhitm3/StudentTest.java
+++ my-app/src/test/java/at/ac/htl/bhitm3/StudentTest.java
@@ -10,7 +10,7 @@ public class StudentTest {
     Student student;
     @Before
     public void setup() {
-        student = new Student();
+        student = new Student("Max","Mustermann", "dd.mm.YYYY", "Salzergasse 7");
     }
     @Test
     public void given_student_ensure_student_is_person() {
