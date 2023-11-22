502095541f64a656a9da523e4d55b8d0fac106d8
IT210190 <113981352+IT210190@users.noreply.github.com>
2023-11-22 14:51:09 +0100

    Update PersonTest

--- my-app/src/test/java/at/ac/htl/bhitm3/PersonTest.java
+++ my-app/src/test/java/at/ac/htl/bhitm3/PersonTest.java
@@ -7,6 +7,28 @@ public class PersonTest {
     Person person;
     @Before
     public void setup() {
-        person = new Person();
+        person = new Person("Max", "Mustermann", "01.01.1970", "Musterstraße 1");
     }
+    
+    @Test
+    public void given_person_ensure_has_firstname() {
+        assert(person.getFirstName().equals("Max"));
+    }
+
+    @Test
+    public void given_person_ensure_has_lastname() {
+        assert(person.getLastName().equals("Mustermann"));
+    }
+
+    @Test
+    public void given_person_ensure_has_dateofbirth() {
+        assert(person.getDateOfBirth().equals("01.01.1970"));
+    }
+
+    @Test
+    public void given_person_ensure_has_address() {
+        assert(person.getAddress().equals("Musterstraße 1"));
+    }
+
+
 }
